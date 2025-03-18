c50ab2e2086d69953bd807121cd9024f36a918b5
Aucemi <164097972+aucemi@users.noreply.github.com>
2025-03-18 20:47:53 -0300

    enviando atividade aluno Aucemi P3A noite

---
+++ src/main/java/br/uniesp/si/techback/controller/UsuarioController.java
@@ -0,0 +1,39 @@
+package br.uniesp.si.techback.controller;
+
+import br.uniesp.si.techback.model.Filme;
+import br.uniesp.si.techback.model.Usuario;
+import br.uniesp.si.techback.service.FilmeService;
+import br.uniesp.si.techback.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/usuario")
+public class UsuarioController {
+
+    @Autowired //Iremos remover essa anotação na próxma aula
+    private UsuarioService service;
+
+    @GetMapping
+    public List<Usuario> listar(){
+        return service.listar();
+    }
+
+    @PostMapping
+    public Usuario inserir(@RequestBody Usuario usuario){
+        return service.cadastrar(usuario);
+    }
+
+    public Usuario alterar(@RequestBody Usuario usuario){
+        return service.alterar(usuario);
+    }
+
+    @DeleteMapping
+    public ResponseEntity delete(Integer id){
+        service.excluir(id);
+        return ResponseEntity.ok().build();
+    }
+}
